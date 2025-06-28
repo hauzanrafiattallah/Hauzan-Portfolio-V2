c35b2e83f4f96beb8ae525b1a50ddcd69cabf6bb
Hauzan Rafi Attallah <rafihauzan42@gmail.com>
2025-06-28 20:50:21 +0700

    feat: add lanyard component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import Lanyard from "./components/Lanyard/Lanyard";
+
 export default function Home() {
   return (
-    <div>
-      <div>Hauzan Ganteng</div>
+    <div className="min-h-screen overflow-x-hidden bg-black">
+      <div className="container border mx-auto h-screen">
+        <div className="grid grid-cols-12">
+          <div className="col-span-6">A</div>
+          <div className="col-span-6">
+            <Lanyard position={[0, 0, 15]} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
